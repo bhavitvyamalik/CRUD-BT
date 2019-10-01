53faf036ef29ecef7bf4caafb61de4fd40afc7b3
Bhavitvya Malik <bhavitvya.malik@gmail.com>
2019-10-01 12:04:52 +0530

    first commit

--- cnn.py
+++ cnn.py
@@ -15,7 +15,7 @@
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
-from keras.layers import Flatten
+from keras.layers import Flatten, BatchNormalization
 from keras.layers import Dense
 
 # Initialising the CNN
@@ -23,6 +23,8 @@ classifier = Sequential()
 
 # Step 1 - Convolution
 classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None)
+
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
@@ -50,16 +52,16 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                    zoom_range = 0.2,
                                    horizontal_flip = True)
 
-test_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale = 1/255.)
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
-                                                 batch_size = 32,
+                                                 batch_size = 64,
                                                  class_mode = 'binary')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
-                                            batch_size = 32,
+                                            batch_size = 64,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
@@ -67,3 +69,5 @@ classifier.fit_generator(training_set,
                          epochs = 25,
                          validation_data = test_set,
                          validation_steps = 2000)
+
+# steps = (epochs * examples) / batch_size
